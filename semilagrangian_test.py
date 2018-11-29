704c0a3e0317d46cf560fe0036949d0fa1b57d93
marquesleandro <marquesleandro67@gmail.com>
2018-11-29 14:40:00 -0200

    semi-lagrangian rodando para varios pontos

--- semilagrangian_test.py
+++ semilagrangian_test.py
@@ -1,3 +1,6 @@
+# V_4 - Semi-Lagrangian Scheme
+# To find the element of several coordinates
+
 # =======================
 # Importing the libraries
 # =======================
@@ -25,7 +28,7 @@ start_time = time()
 
 name_mesh = 'semilagrangian_test.msh'
 number_equations = 3
-mesh = trimsh.Linear('/home/marquesleandro/fem/mesh',name_mesh, number_equations)
+mesh = trimsh.Linear('/home/marquesleandro/mesh',name_mesh, number_equations)
 mesh.ien()
 mesh.coord()
 
@@ -33,96 +36,85 @@ end_time = time()
 print 'time duration: %.1f seconds' %(end_time - start_time)
 print ""
 
-
+#-----------------------------------------
+# varios pontos
 points1_x = np.zeros([3,1], dtype = float)
 points1_y = np.zeros([3,1], dtype = float)
 
-#200
-points1_x[0] = 0.5
-points1_y[0] = 0.4
+#217
+points1_x[0] = 5.9
+points1_y[0] = 0.3
 
 #187
-points1_x[1] = 2.2
+points1_x[1] = 2.3
 points1_y[1] = 0.7
 
 #261
 points1_x[2] = 3.0
 points1_y[2] = 0.5
 
-# node 62
-node = 62
-x = 1.4
-y = 0.8
 
+for node in range(0,3): #range mesh.npoints
+ x = float(points1_x[node])
+ y = float(points1_y[node])
 
-element_avg = []
-
-alpha = np.array([10.0,10.0,10.0])
-
-while True:
- for e in mesh.neighbors_elements[node]:
-  v1 = mesh.IEN[e][0]
-  v2 = mesh.IEN[e][1]
-  v3 = mesh.IEN[e][2]
-
-  x1 = float(mesh.x[v1])
-  x2 = float(mesh.x[v2])
-  x3 = float(mesh.x[v3])
-
-  y1 = float(mesh.y[v1])
-  y2 = float(mesh.y[v2])
-  y3 = float(mesh.y[v3])
-  
-#  x = float(points1_x[i])
-#  y = float(points1_y[i])
-
-  A = np.array([[x1,x2,x3],
-                [y1,y2,y3],
-                [1.0,1.0,1.0]])
-
-  b = np.array([x,y,1.0])
-
-  alpha = np.linalg.solve(A,b)
-
-  avg = np.average(np.sqrt(alpha**2))
-  aa2 = [e, avg]
-  element_avg.append(aa2) 
-
-
-  if np.all(alpha > 0.0) and np.all(alpha < 1.0):
-   ee = e + 175
-   print ee
-   break
+ length = []
+ ww = 1
  
- near_element = min(element_avg, key=lambda k:k[1])
- near_element = near_element[0]
- 
- v1 = mesh.IEN[near_element][0]
- v2 = mesh.IEN[near_element][1]
- v3 = mesh.IEN[near_element][2]
+ print node
 
- x1 = float(mesh.x[v1])
- x2 = float(mesh.x[v2])
- x3 = float(mesh.x[v3])
+ while ww == 1:
+#for i in range(0,3):
 
- y1 = float(mesh.y[v1])
- y2 = float(mesh.y[v2])
- y3 = float(mesh.y[v3])
+  for e in mesh.neighbors_elements[node]:
+   v1 = mesh.IEN[e][0]
+   v2 = mesh.IEN[e][1]
+   v3 = mesh.IEN[e][2]
+
+   x1 = float(mesh.x[v1])
+   x2 = float(mesh.x[v2])
+   x3 = float(mesh.x[v3])
+
+   y1 = float(mesh.y[v1])
+   y2 = float(mesh.y[v2])
+   y3 = float(mesh.y[v3])
   
- x_a = x1 - x
- x_b = x2 - x
- x_c = x3 - x
-  
- y_a = y1 - x
- y_b = y2 - x
- y_c = y3 - x
+   A = np.array([[x1,x2,x3],
+                 [y1,y2,y3],
+                 [1.0,1.0,1.0]])
+
+   b = np.array([x,y,1.0])
+
+   alpha = np.linalg.solve(A,b)
+   
+   if np.all(alpha >= 0.0) and np.all(alpha <= 1.0):
+    ee = e + 175
+    print ee 
+    ww = 0
+    break
+
+   else:
+    x_a = x1 - x
+    x_b = x2 - x
+    x_c = x3 - x
+   
+    y_a = y1 - y
+    y_b = y2 - y
+    y_c = y3 - y
  
- length1 = np.sqrt(x_a**2 + y_a**2)
- length2 = np.sqrt(x_b**2 + y_b**2)
- length3 = np.sqrt(x_c**2 + y_c**2)
+    length1 = np.sqrt(x_a**2 + y_a**2)
+    length2 = np.sqrt(x_b**2 + y_b**2)
+    length3 = np.sqrt(x_c**2 + y_c**2)
 
- lenght = [[v1,length1],[v2,length2],[v3,length3]]
- lenght_min = min(lenght, key=lambda k:k[1])
- node = lenght_min[0]
-
-
+    a_1 = [v1,length1]
+    a_2 = [v2,length2]
+    a_3 = [v3,length3]
+ 
+    length.append(a_1)
+    length.append(a_2)
+    length.append(a_3)
+   
+    ww = 1
+ 
+  length_min = min(length, key=lambda k:k[1])
+  node = length_min[0]
