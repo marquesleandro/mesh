7312ca1ad31b9827106f418fcc7a3cd24be167b1
marquesleandro <marquesleandro67@gmail.com>
2018-12-03 16:01:44 -0200

    convex surface validated

--- semilagrangian_test_v2.py
+++ semilagrangian_test_v2.py
@@ -1,6 +1,6 @@
 # V_2 - Semi-Lagrangian Scheme
 # To find the element of several coordinates
-# and outside coordinates with once time solve
+# and outside coordinates with convex surface
 
 # =======================
 # Importing the libraries
@@ -27,8 +27,8 @@ print '------------'
 
 start_time = time()
 
-name_mesh = 'semilagrangian_test.msh'
-number_equations = 3
+name_mesh = 'semilagrangian_test2.msh'
+number_equations = 1
 mesh = trimsh.Linear('/home/marquesleandro/mesh',name_mesh, number_equations)
 mesh.ien()
 mesh.coord()
@@ -52,78 +52,71 @@ points1_x = np.zeros([9,1], dtype = float)
 points1_y = np.zeros([9,1], dtype = float)
 
 #node 0
-#204 elemento vizinho proximo do ponto 1
-points1_x[0] = 0.1
-points1_y[0] = 0.8
+#238 elemento vizinho proximo do ponto 1
+points1_x[0] = 0.23
+points1_y[0] = 0.97
 
 #node 1
-#180 elemento vizinho distante do ponto 2
-points1_x[1] = 2.0
-points1_y[1] = 0.7
+#490 elemento vizinho distante do ponto 2
+points1_x[1] = 0.24
+points1_y[1] = 0.44
 
 #node 2
-#3 noh no contorno imovel
-points1_x[2] = 8.275
-points1_y[2] = 1.0
+#334 elemento mega distante do ponto 3
+points1_x[2] = 0.14
+points1_y[2] = 0.12
 
 #node 3
-#4 noh no vertice imovel
-points1_x[3] = 10.0
-points1_y[3] = 1.0
+#277 elemento vizinho mega distante do ponto 4 colado no contorno
+points1_x[3] = 0.0
+points1_y[3] = 0.21
 
 #node 4
-#5 noh vizinho proximo do ponto 5 fora do dominio
-points1_x[4] = 10.1
-points1_y[4] = 0.1
+#noh 5 no vertice imovel - elemento 587
+points1_x[4] = 0.0
+points1_y[4] = 0.0
 
 #node 5
-#7 noh vizinho distante do ponto 6 fora do dominio
-points1_x[5] = 0.5
-points1_y[5] = 1.1
+#noh vizinho proximo do ponto 6 fora do dominio
+points1_x[5] = 0.02
+points1_y[5] = 1.05
 
 #node 6
-#36 noh vizinho mega distante do ponto 7 fora do dominio
-points1_x[6] = 8.4
-points1_y[6] = -0.1
+#noh 7 no contorno imovel - elemento 51
+points1_x[6] = 0.25
+points1_y[6] = 0.95
 
 #node 7
-#228 elemento vizinho mega distante do ponto 8
-points1_x[7] = 9.2
-points1_y[7] = 0.7
+#285 elemento mega distante do ponto 8 com uma superficie convexa
+points1_x[7] = 0.67
+points1_y[7] = 0.47
 
 #node 8
-#317 elemento vizinho mega distante do ponto 9 colado no contorno
-points1_x[8] = 5.8
-points1_y[8] = 0.1
+#noh mega distante proximo do ponto 36 fora do dominio com uma superficie convexa
+points1_x[8] = 1.2
+points1_y[8] = 0.15
 
-
-
-'''
-#node
-node = 61
-x = 9.1
-y = 0.5
-'''
 
 
 for node in range(0,9): #range mesh.npoints
  x = float(points1_x[node])
  y = float(points1_y[node])
 
+ ii = 0
+ IEN_neighbors = np.zeros([len(mesh.neighbors_elements),3], dtype = float)
  length = []
  ww = 1
- 
+ print ""
  print node
 
-#length = []
-#ww = 1
  while ww == 1:
-#for i in range(0,3):
-
   for e in mesh.neighbors_elements[node]:
    v1 = mesh.IEN[e][0]
    v2 = mesh.IEN[e][1]
    v3 = mesh.IEN[e][2]
+
+   IEN_neighbors[ii] = [v1,v2,v3]
+   ii = ii + 1
 
    x1 = float(mesh.x[v1])
    x2 = float(mesh.x[v2])
@@ -142,7 +135,7 @@ for node in range(0,9): #range mesh.npoints
    alpha = np.linalg.solve(A,b)
  
    if np.all(alpha >= 0.0) and np.all(alpha <= 1.0):
-    ee = e + 175
+    ee = e + 11
     print "elemento dominio %s" %ee
     print "fazer interpolacao triangular" 
     ww = 0
@@ -170,21 +163,26 @@ for node in range(0,9): #range mesh.npoints
     length.append(a_3)
    
     ww = 1
- 
+
+  # first neighbor is element found 
   if ww == 0:
     break
   
+  # coordinate doesn't found
   else:
    length_min = min(length, key=lambda k:k[1])
    node1 = node
    node = length_min[0]
+   print node
 
+   # outside domain
    if node == node1 and ww == 1:
     node = node + 1
     print "elemento contorno proximo ao no %s" %node
     print "fazer regra da alavanca"
     ww = 0
     break
+
 
 end_time = time()
 print ""
