119e256bc51694a8f00b918ba60b22b9d2874536
Leandro Marques <marquesleandro67@gmail.com>
2020-01-09 15:49:44 -0300

    modif

--- semilagrangian_test.py
+++ semilagrangian_test.py
@@ -26,9 +26,9 @@ start_time = time()
 
 name_mesh = 'semilagrangian_test2.msh'
 number_equations = 1
-mesh = import_msh.Linear('/home/marquesleandro/mesh',name_mesh, number_equations)
-mesh.ien()
+mesh = import_msh.Linear2D('/home/marquesleandro/mesh',name_mesh, number_equations)
 mesh.coord()
+mesh.ien()
 
 end_time = time()
 print 'time duration: %.1f seconds' %(end_time - start_time)
@@ -45,136 +45,155 @@ start_time = time()
 
 
 # varios pontos
-points1_x = np.zeros([9,1], dtype = float)
-points1_y = np.zeros([9,1], dtype = float)
+points1_x = np.zeros([6,1], dtype = float)
+points1_y = np.zeros([6,1], dtype = float)
 
-#node 0
-#238 elemento vizinho proximo do ponto 1
-points1_x[0] = 0.23
-points1_y[0] = 0.97
-
-#node 1
-#490 elemento vizinho distante do ponto 2
-points1_x[1] = 0.24
-points1_y[1] = 0.44
-
-#node 2
-#334 elemento mega distante do ponto 3
-points1_x[2] = 0.14
-points1_y[2] = 0.12
-
-#node 3
-#277 elemento vizinho mega distante do ponto 4 colado no contorno
-points1_x[3] = 0.0
-points1_y[3] = 0.21
-
-#node 4
-#noh 5 no vertice imovel - elemento 587
-points1_x[4] = 0.0
-points1_y[4] = 0.0
-
-#node 5
-#noh vizinho proximo do ponto 6 fora do dominio
-points1_x[5] = 0.02
-points1_y[5] = 1.05
-
-#node 6
-#noh 7 no contorno imovel - elemento 51
-points1_x[6] = 0.25
-points1_y[6] = 0.95
-
-#node 7
-#285 elemento mega distante do ponto 8 com uma superficie convexa
-points1_x[7] = 0.67
-points1_y[7] = 0.47
-
-#node 8
-#noh mega distante proximo do ponto 36 fora do dominio com uma superficie convexa
-points1_x[8] = 1.2
-points1_y[8] = 0.15
+#no 0 -> proximo ao 11
+points1_x[0] = 1.1
+points1_y[0] = 0.4
 
 
+#no 1 -> proximo ao 23
+points1_x[1] = 0.4
+points1_y[1] = 0.3
 
-for i in range(0,9): #range mesh.npoints
+#no 2 -> proximo ao 5
+points1_x[2] = 0.0
+points1_y[2] = 1.0
+
+#no 3 -> proximo ao 32
+points1_x[3] = 0.8
+points1_y[3] = 0.1
+
+#no 4 -> proximo ao 12
+points1_x[4] = 1.1
+points1_y[4] = 0.2
+
+#no 5 -> proximo ao 28
+points1_x[5] = 0.1
+points1_y[5] = 0.9
+
+for i in range(0,6): #range mesh.npoints
  x = float(points1_x[i])
  y = float(points1_y[i])
-
+ 
  node = i
  length = []
- ww = 1
- print ""
- print node
+ barycentric = []
+ element = []
+ breaking = 0
 
- while ww == 1:
-  for e in mesh.neighbors_elements[node]:
-   v1 = mesh.IEN[e][0]
-   v2 = mesh.IEN[e][1]
-   v3 = mesh.IEN[e][2]
-
-   x1 = float(mesh.x[v1])
-   x2 = float(mesh.x[v2])
-   x3 = float(mesh.x[v3])
-
-   y1 = float(mesh.y[v1])
-   y2 = float(mesh.y[v2])
-   y3 = float(mesh.y[v3])
-  
-   A = np.array([[x1,x2,x3],
-                 [y1,y2,y3],
-                 [1.0,1.0,1.0]])
-
-   b = np.array([x,y,1.0])
-
-   alpha = np.linalg.solve(A,b)
+ while breaking == 0:
+  for j in mesh.far_neighbors_nodes[node]:
+   xx = float(mesh.x[j])
+   yy = float(mesh.y[j])
  
-   if np.all(alpha >= 0.0) and np.all(alpha <= 1.0):
-    ee = e + 11
-    print "elemento dominio %s" %ee
-    print "fazer interpolacao triangular" 
-    ww = 0
+   x_a = xx - x
+   y_a = yy - y
+  
+   length1 = np.sqrt(x_a**2 + y_a**2)
+
+   a_1 = [j,length1]
+ 
+   length.append(a_1)
+   
+  length_min = min(length, key=lambda k:k[1])
+  node1 = node
+  node = length_min[0]
+
+  # node more short
+  if node == node1:
+   for e in mesh.neighbors_elements[node]:
+    v1 = mesh.IEN[e][0]
+    v2 = mesh.IEN[e][1]
+    v3 = mesh.IEN[e][2]
+
+    x1 = float(mesh.x[v1])
+    x2 = float(mesh.x[v2])
+    x3 = float(mesh.x[v3])
+
+    y1 = float(mesh.y[v1])
+    y2 = float(mesh.y[v2])
+    y3 = float(mesh.y[v3])
+  
+    A = np.array([[x1,x2,x3],
+                  [y1,y2,y3],
+                  [1.0,1.0,1.0]])
+
+    b = np.array([x,y,1.0])
+ 
+    alpha = np.linalg.solve(A,b)
+
+    barycentric.append(alpha)
+    element.append(e)
+
+   barycentric = np.array(barycentric)
+   idx = (barycentric >= 0.0) & (barycentric <= 1.0)
+   aa = np.where((idx == True).all(axis=1))
+   print idx
+   print aa
+
+   # inside element
+   try:
+    aa = int(aa[0])
+    e = element[aa]
+
+    v1 = mesh.IEN[e][0]
+    v2 = mesh.IEN[e][1]
+    v3 = mesh.IEN[e][2]
+
+    x1 = float(mesh.x[v1])
+    x2 = float(mesh.x[v2])
+    x3 = float(mesh.x[v3])
+
+    y1 = float(mesh.y[v1])
+    y2 = float(mesh.y[v2])
+    y3 = float(mesh.y[v3])
+  
+
+    A1 = 0.5*np.linalg.det(np.array([[1, x, y],
+                                     [1, x2, y2],
+                                     [1, x3, y3]]))
+ 
+    A2 = 0.5*np.linalg.det(np.array([[1, x1, y1],
+                                     [1, x, y],
+                                     [1, x3, y3]]))
+ 
+    A3 = 0.5*np.linalg.det(np.array([[1, x1, y1],
+                                     [1, x2, y2],
+                                     [1, x, y]]))
+ 
+    At = 0.5*np.linalg.det(np.array([[1, x1, y1],
+                                     [1, x2, y2],
+                                     [1, x3, y3]]))
+   
+    L1 = A1/At
+    L2 = A2/At
+    L3 = A3/At
+     
+    N1 = L1
+    N2 = L2
+    N3 = L3
+
+    
+    print "interpolacao no %s" %node
+    print ""
+
+    breaking = 1
     break
 
-   else:
-    x_a = x1 - x
-    x_b = x2 - x
-    x_c = x3 - x
-   
-    y_a = y1 - y
-    y_b = y2 - y
-    y_c = y3 - y
- 
-    length1 = np.sqrt(x_a**2 + y_a**2)
-    length2 = np.sqrt(x_b**2 + y_b**2)
-    length3 = np.sqrt(x_c**2 + y_c**2)
-
-    a_1 = [v1,length1]
-    a_2 = [v2,length2]
-    a_3 = [v3,length3]
- 
-    length.append(a_1)
-    length.append(a_2)
-    length.append(a_3)
-   
-    ww = 1
-
-  # first neighbor is element found 
-  if ww == 0:
+    # outside domain
+   except TypeError:
+    print "fora dominio no %s" %node
+    print ""
+    
+    breaking = 1
     break
-  
-  # coordinate doesn't found
+
+  # node far yet
   else:
-   length_min = min(length, key=lambda k:k[1])
-   node1 = node
-   node = length_min[0]
-   print node
+   breaking = 0
 
-   # outside domain
-   if node == node1 and ww == 1:
-    node = node + 1
-    print "elemento contorno proximo ao no %s" %node
-    print "fazer regra da alavanca"
-    ww = 0
-    break
 
 
 end_time = time()
